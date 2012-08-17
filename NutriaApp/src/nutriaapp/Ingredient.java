1840691dfcf52e92c9b594a2e9d76f5d9769b87d
Ariel Ayaviri <ariel@kplian.com>
2012-08-17 09:16:59 -0400

    Created Pojos Nutrient and Ingredient

---
+++ NutriaApp/src/nutriaapp/Ingredient.java
@@ -0,0 +1,95 @@
+package nutriaapp;
+/**
+ *
+ * @author Ariel
+ */
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+public class Ingredient implements Serializable {
+    private Long id;
+    private String name;
+    private Double price;
+    private Double roundingFactor;
+    private List<Nutrient> nutrients;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getRoundingFactor() {
+        return roundingFactor;
+    }
+
+    public void setRoundingFactor(Double roundingFactor) {
+        this.roundingFactor = roundingFactor;
+    }
+
+    public List<Nutrient> getNutrients() {
+        return nutrients;
+    }
+
+    public void setNutrients(List<Nutrient> nutrients) {
+        this.nutrients = nutrients;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" + "id=" + id + ", name=" + name + ", price=" + price + ", roundingFactor=" + roundingFactor + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.id);
+        hash = 37 * hash + Objects.hashCode(this.name);
+        hash = 37 * hash + Objects.hashCode(this.price);
+        hash = 37 * hash + Objects.hashCode(this.roundingFactor);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Ingredient other = (Ingredient) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.price, other.price)) {
+            return false;
+        }
+        if (!Objects.equals(this.roundingFactor, other.roundingFactor)) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ NutriaApp/src/nutriaapp/Nutrient.java
@@ -0,0 +1,74 @@
+package nutriaapp;
+
+/**
+ *
+ * @author Ariel
+ */
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Nutrient implements Serializable {
+    private Long id;
+    private String name;
+    private String unit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return "Nutrient{" + "id=" + id + ", name=" + name + ", unit=" + unit + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 73 * hash + Objects.hashCode(this.id);
+        hash = 73 * hash + Objects.hashCode(this.name);
+        hash = 73 * hash + Objects.hashCode(this.unit);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Nutrient other = (Nutrient) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.unit, other.unit)) {
+            return false;
+        }
+        return true;
+    }
+}
